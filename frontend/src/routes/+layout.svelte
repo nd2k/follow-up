df73ad482101596db3a102cff4db72e13534cd91
Nico <nicolas.decat@gmail.com>
2026-09-18 14:43:35 +0200

    Adapt vite.config to deploy

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 	import favicon from '#lib/assets/favicon.svg';
 
+	export const ssr = false;
+	export const prerender = false;
+
 	let { children } = $props();
 </script>
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapterStatic from '@sveltejs/adapter-static';
 
 export default defineConfig({
 	plugins: [
@@ -14,7 +15,9 @@ export default defineConfig({
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter(),
+			adapter: adapterStatic({
+				fallback: 'index.html',
+			}),
 			experimental: { remoteFunctions: true }
 		})
 	]
