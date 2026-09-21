2f8bd292d8bf8784d4c8c6484511e629614c1e0a
Nico <nicolas.decat@gmail.com>
2026-09-21 14:59:14 +0200

    add babies management

--- backend/src/main/java/com/nd2k/follow_up/baby/out/persistence/UserBabyEntity.java
+++ backend/src/main/java/com/nd2k/follow_up/baby/out/persistence/UserBabyEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "user_baby", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "baby_id"}))
 public class UserBabyEntity {
 
+    @SuppressWarnings("unused")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
